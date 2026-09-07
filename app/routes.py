7d5b79ba8996df92f84677c479ca2392e82223c6
Hemanjali Lakku <hemaanji287@gmail.com>
2026-09-07 20:03:42 +0530

    Add registration login and dashboard

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,69 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect
+from database import get_db_connection
 
 main = Blueprint("main", __name__)
+
 
 @main.route("/")
 def home():
     return render_template("index.html")
-@main.route("/login")
+
+
+@main.route("/login", methods=["GET", "POST"])
 def login():
+
+    if request.method == "POST":
+
+        email = request.form["email"]
+        password = request.form["password"]
+
+        conn = get_db_connection()
+
+        user = conn.execute(
+            "SELECT * FROM users WHERE email = ? AND password = ?",
+            (email, password)
+        ).fetchone()
+
+        conn.close()
+
+        if user:
+            return redirect("/dashboard")
+
+        return "Invalid email or password"
+
     return render_template("login.html")
-@main.route("/register")
+
+
+@main.route("/register", methods=["GET", "POST"])
 def register():
+
+    if request.method == "POST":
+
+        name = request.form["name"]
+        email = request.form["email"]
+        password = request.form["password"]
+
+        conn = get_db_connection()
+
+        try:
+            conn.execute(
+                "INSERT INTO users (name, email, password) VALUES (?, ?, ?)",
+                (name, email, password)
+            )
+
+            conn.commit()
+
+        except Exception as e:
+            conn.close()
+            return f"Registration failed: {e}"
+
+        conn.close()
+
+        return redirect("/login")
+
     return render_template("register.html")
+
+
+@main.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
